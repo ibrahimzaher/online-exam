7baae73ff9beee84df35648949a75f43cc7771b2
ibrahimzaher <hemazaher15@gmail.com>
2025-11-06 06:51:16 -0800

    init , install packages  and styling

---
+++ src/app/app.config.ts
@@ -0,0 +1,34 @@
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
+import Aura from '@primeuix/themes/aura';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideHttpClient(withFetch()),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: '.dark',
+          cssLayer: {
+            name: 'primeng',
+            order: 'theme, base, primeng',
+          },
+        },
+      },
+    }),
+  ],
+};
