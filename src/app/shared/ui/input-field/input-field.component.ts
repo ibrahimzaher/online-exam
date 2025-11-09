872dc7b23b2d18c5136a949471f399e30d51d99a
ibrahimzaher <hemazaher15@gmail.com>
2025-11-09 14:38:48 -0800

    Add FormErrorComponent and use it in InputFieldComponent for component separation

--- src/app/shared/ui/input-field/input-field.component.ts
+++ src/app/shared/ui/input-field/input-field.component.ts
@@ -1,21 +1,20 @@
 import { Component, input } from '@angular/core';
 import { FormControl, AbstractControl, ReactiveFormsModule } from '@angular/forms';
-import { MessageModule } from 'primeng/message';
 import { PasswordModule } from 'primeng/password';
 import { InputTextModule } from 'primeng/inputtext';
 import { CommonModule } from '@angular/common';
 import { NgxIntlTelInputModule } from 'ngx-intl-tel-input';
-import { getErrorMessage } from '../../utils/form-error.utils';
+import { FormErrorComponent } from './components/form-error/form-error.component';
 
 @Component({
   selector: 'app-input-field',
   imports: [
     PasswordModule,
-    MessageModule,
     InputTextModule,
     ReactiveFormsModule,
     CommonModule,
     NgxIntlTelInputModule,
+    FormErrorComponent,
   ],
   templateUrl: './input-field.component.html',
   styleUrl: './input-field.component.css',
@@ -29,22 +28,10 @@ export class InputFieldComponent {
   feedback = input<boolean>(false);
   toggleMask = input<boolean>(true);
   otp = input<boolean>(false);
-  get hasError(): boolean {
-    return !!(this.controller().errors && this.controller().touched);
-  }
   get control(): FormControl {
     return this.controller() as FormControl;
   }
-  get errorMessages(): string[] {
-    const errors = this.controller().errors;
-    if (!errors) return [];
-
-    return Object.keys(errors).map((key) =>
-      getErrorMessage(key, errors[key], {
-        label: this.label(),
-        otp: this.otp(),
-        type: this.type(),
-      })
-    );
+  get hasError(): boolean {
+    return !!(this.controller().errors && this.controller().touched);
   }
 }
