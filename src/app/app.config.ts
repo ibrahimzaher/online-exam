000245c110059521feba809d1f9725f1f526db38
ibrahimzaher <hemazaher15@gmail.com>
2025-11-11 13:44:37 -0800

    Install package @izaher-dev/auth ,Add Environment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,6 +10,8 @@ import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
+import { AUTH_PROVIDERS, API_CONFIG } from '@izaher-dev/auth'
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -18,6 +20,8 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withFetch()),
     provideRouter(routes),
     provideAnimationsAsync(),
+    ...AUTH_PROVIDERS,
+    { provide: API_CONFIG, useValue: { baseUrl: environment.baseUrl } },
     providePrimeNG({
       theme: {
         preset: Aura,
