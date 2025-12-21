e2931b574fd1b06d3fa985f660637e31d7d6d2a9
ibrahimzaher <hemazaher15@gmail.com>
2025-12-22 01:20:34 +0200

    fix: resolve mobile layout issues

--- src/app/core/layout/dashboard-layout/dashboard-layout.component.ts
+++ src/app/core/layout/dashboard-layout/dashboard-layout.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { AvatarModule } from 'primeng/avatar';
 import { ButtonModule } from 'primeng/button';
 import { DrawerModule } from 'primeng/drawer';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { filter } from 'rxjs';
 @Component({
   selector: 'app-dashboard-layout',
   imports: [
@@ -20,4 +21,10 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 })
 export class DashboardLayoutComponent {
   visible = false;
+  router = inject(Router);
+  constructor() {
+    this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe(() => {
+      this.visible = false;
+    });
+  }
 }
