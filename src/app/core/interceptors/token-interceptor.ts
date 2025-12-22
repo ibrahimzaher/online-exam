eec0175bd6be645fa678a515380f58a6a839180b
ibrahimzaher <hemazaher15@gmail.com>
2025-12-22 12:44:21 +0200

    fix(auth): redirect to login when auth token is missing refactor(ui): improve responsive design

--- src/app/core/interceptors/token-interceptor.ts
+++ src/app/core/interceptors/token-interceptor.ts
@@ -1,11 +1,21 @@
 import { HttpHeaders, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { StorageService } from '../services/storage.service';
-import { tap } from 'rxjs';
+import { catchError, tap, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
   const _storage = inject(StorageService);
   const token = _storage.getItem<string>('token');
   const authReq = token ? req.clone({ setHeaders: { token: token } }) : req;
-  return next(authReq)
+  const router = inject(Router);
+  return next(authReq).pipe(
+    catchError((err) => {
+      if (err.status == 401) {
+        localStorage.removeItem('token');
+        router.navigate(['/login']);
+      }
+      return throwError(() => err);
+    })
+  );
 };
